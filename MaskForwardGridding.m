35742f1204dd39f8336a90ce071ca4cbafd5b763
bbacsem <semipark104508@gmail.com>
2022-11-23 09:26:11 +0900

    clearvars 없애기

--- MaskForwardGridding.m
+++ MaskForwardGridding.m
@@ -2,10 +2,8 @@ function  [IMGF] = MaskForwardGridding(mc_kdata, coilsen, kerneldistance, xyz_in
 
 xyz_index_gpu = gpuArray(double(xyz_index));
 kerneldistance_gpu = gpuArray(kerneldistance);
-IMGF = [];
-
+IMGF = zeros(matrixsize,matrixsize,matrixsize,'single');
 ncoils = size(mc_kdata,2);
-IMGCoil = zeros(matrixsize,matrixsize,matrixsize,ncoils,'single');
 
 % xyz_index = cat(2,x_index,y_index,z_index);
 mask_gpu = accumarray(xyz_index_gpu,kerneldistance_gpu,[matrixsize matrixsize matrixsize]);
@@ -13,7 +11,6 @@ idx_mask_gpu = find(mask_gpu ~= 0);
 mask_num_gpu = mask_gpu(idx_mask_gpu)+10*eps;
 
 for i = 1:ncoils
-
 kdata = gpuArray(repmat(reshape(squeeze(mc_kdata(:,i,:)),[],1),[64 1]));
 kdata = kdata(index_smth2);
 final_value = kerneldistance_gpu.*kdata;
@@ -27,7 +24,6 @@ IMG = fftshift(fft(fftshift(IMG,3),[],3),3);
 
 IMGF = IMGF + IMG.*gpuArray(coilsen(:,:,:,i));
 end
-
 % clearvars -except coilsen IMGCoil win_3d matrixsize 
 % IMGF = sum(coilsen.*IMGCoil,4);
 % IMGF = sqrt(sum(abs(IMGCoil).^2,4));
